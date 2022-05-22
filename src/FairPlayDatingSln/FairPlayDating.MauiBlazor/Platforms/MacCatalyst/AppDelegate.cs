e9f55c3c3ade2e2b74bc613fd0de29e23c361205
Eduardo Fonseca <efonsecab@gmail.com>
2022-05-22 09:52:54 -0600

    Adding Base for css-grid based layout

---
+++ src/FairPlayDatingSln/FairPlayDating.MauiBlazor/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FairPlayDating.MauiBlazor
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
