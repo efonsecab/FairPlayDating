36e61bfba647394c8f7adb34425f205128573b1f
Eduardo Fonseca <efonsecab@gmail.com>
2022-05-23 06:46:10 -0600

    Adding controllers generator

--- src/FairPlayDatingSln/FairPlayDating/Server/Controllers/ActivityController.cs
+++ src/FairPlayDatingSln/FairPlayDating/Server/Controllers/ActivityController.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using FairPlayDating.Common.CustomAttributes;
+using FairPlayDating.Common.Global;
 using FairPlayDating.DataAccess.Data;
 using FairPlayDating.DataAccess.Models;
 using FairPlayDating.Models.Activity;
@@ -16,26 +18,8 @@ namespace FairPlayDating.Server.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [Authorize]
-    public class ActivityController : ControllerBase
+    [ControllerOfEntity(entityName:Constants.EntityNames.Activity, primaryKeyType:typeof(long))]
+    public partial class ActivityController : ControllerBase
     {
-        private IMapper _mapper;
-        private ActivityService _activityService;
-
-        public ActivityController(ActivityService activityService, 
-            IMapper mapper)
-        {
-            _mapper = mapper;
-            _activityService = activityService;
-        }
-
-        [HttpGet("[action]")]
-        public async Task<ActivityModel[]> GetAllActivities(CancellationToken cancellationToken)
-        {
-            var result = await this._activityService
-                .GetAllActivity(trackEntities:false, cancellationToken: cancellationToken)
-                .Select(p=> _mapper.Map<Activity,ActivityModel>(p))
-                .ToArrayAsync(cancellationToken: cancellationToken);
-            return result;
-        }
     }
 }
