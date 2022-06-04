fc8971d5ec52394d4e8d35cbc8512c3f88cbbd47
Eduardo Fonseca <efonsecab@gmail.com>
2022-06-04 16:00:24 -0600

    Enabling suport for spatial columns in EF Core. Adding CurrentGeoLocation column.

---
+++ src/FairPlayDatingSln/FairPlayDating.DataAccess/Models/UserProfile.partial.cs
@@ -0,0 +1,17 @@
+﻿using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayDating.DataAccess.Models
+{
+    public partial class UserProfile
+    {
+        //Check https://docs.microsoft.com/en-us/ef/core/modeling/spatial
+        [Required]
+        public Point CurrentGeoLocation { get; set; }
+    }
+}
--- src/FairPlayDatingSln/FairPlayDating.Database/dbo/Tables/ApplicationUserVouch.sql
+++ src/FairPlayDatingSln/FairPlayDating.Database/dbo/Tables/ApplicationUserVouch.sql
@@ -3,5 +3,9 @@
 	[ApplicationUserVouchId] BIGINT NOT NULL CONSTRAINT PK_ApplicationUserVouch PRIMARY KEY,
 	[FromApplicationUserId] BIGINT NOT NULL,
 	[ToApplicationUserId] BIGINT NOT NULL,
-	[Description] NVARCHAR(500) NOT NULL
+	[Description] NVARCHAR(500) NOT NULL,
+	[RowCreationDateTime] DATETIMEOFFSET NOT NULL, 
+    [RowCreationUser] NVARCHAR(256) NOT NULL,
+    [SourceApplication] NVARCHAR(250) NOT NULL, 
+    [OriginatorIPAddress] NVARCHAR(100) NOT NULL
 )
